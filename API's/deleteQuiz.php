ef6333fea09b82c74ab70639654ad54ec2cf5cbe
Rb-112594 <reboureslan@gmail.com>
2025-04-27 21:24:31 +0300

    modified delete quiz to add a condition

--- API's/deleteQuiz.php
+++ API's/deleteQuiz.php
@@ -10,4 +10,24 @@ if (!empty($data->quiz_id)) {
 } else {
     echo json_encode(["success" => false, "message" => "Missing fields."]);
 }
+?><?php
+include("./connection.php");
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (!empty($data->quiz_id)) {
+    $checkQuiz = $connection->prepare("SELECT * FROM quizzes WHERE quiz_id = ?");
+    $checkQuiz->execute([$data->quiz_id]);
+    $quiz = $checkQuiz->fetch(PDO::FETCH_ASSOC);
+    
+    if ($quiz) {
+        $deleteQuiz = $connection->prepare("DELETE FROM quizzes WHERE quiz_id = ?");
+        $deleteQuiz->execute([$data->quiz_id]);
+        echo json_encode(["success" => true, "message" => "Quiz deleted successfully."]);
+    } else {
+        echo json_encode(["success" => false, "message" => "Quiz not found."]);
+    }
+} else {
+    echo json_encode(["success" => false, "message" => "Missing fields."]);
+}
 ?>
