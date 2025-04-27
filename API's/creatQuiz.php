597068bf01c9987b10244721d5a9b922101834cd
Rb-112594 <reboureslan@gmail.com>
2025-04-27 21:11:07 +0300

    Adding a condition on creating quiz API

--- API's/creatQuiz.php
+++ API's/creatQuiz.php
@@ -4,9 +4,25 @@ include("./connection.php");
 $data = json_decode(file_get_contents("php://input"));
 
 if (!empty($data->title)) {
-    $createQuiz = $connection->prepare("INSERT INTO quizzes (title, description) VALUES (?, ?)");
-    $createQuiz->execute([$data->title, $data->description ?? ""]);
-    echo json_encode(["success" => true, "quiz_id" => $connection->lastInsertId()]);
+  
+    $checkQuiz = $connection->prepare("SELECT * FROM quizzes WHERE title = ?");
+    $checkQuiz->execute([$data->title]);
+    $existingQuiz = $checkQuiz->fetch(PDO::FETCH_ASSOC);
+
+    if ($existingQuiz) {
+    
+        echo json_encode(["success" => false, "message" => "Quiz with this title already exists."]);
+    } else {
+     
+        $createQuiz = $connection->prepare("INSERT INTO quizzes (title, description) VALUES (?, ?)");
+        $createQuiz->execute([$data->title, $data->description ?? ""]);
+
+        echo json_encode([
+            "success" => true,
+            "quiz_id" => $connection->lastInsertId(),
+            "message" => "A quiz with title: '{$data->title}' and description: '{$data->description}' was created."
+        ]);
+    }
 } else {
     echo json_encode(["success" => false, "message" => "Missing fields."]);
 }
